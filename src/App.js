b8f47cdfe0da4ad94b637f617d22feae65477676
estherkimyunjung <estherkimyunjung@gmail.com>
2020-07-18 02:32:50 -0500

    test react route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-// import LogIn from './components/auth/LogIn'
+import Login from './components/auth/LoginModal'
+import SignUp from './components/auth/SignUp'
+import NavBar from './containers/NavBar'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import './styles.css'
 import {
   Button,
@@ -19,13 +22,21 @@ class App extends React.Component {
   render(){
 
     return (
-  
-    <div class="bg-img">
-      Log In
-    </div>
-    
-
-       
+      <BrowserRouter>
+        <NavBar/>
+        <Switch>
+          <Route path='/signup' component={SignUp}/>
+          <Route path='/login' component={Login}/>
+          {/* component about us / contact us */}
+          {/* <Route exact path='/houses' render={() => <HouseDisplay houses={this.state.houses}/> with props*/}
+          {/* <Route path='/houses' render={(routerProps) => <HouseDisplay {...routerProps}houses={this.state.houses}/> with routerProps and props*/}
+          {/* <Route path='/houses/:id' component={HouseDiplay}/> without props but info component currnet house id is: {this.props.match.params.id}*/}
+          {/* <Route path='/houses/new' render={(routerProps) => <HouseForm {...routerProps} addHouse={this.addHouse}/> */}
+          {/* <Link to='/home'>Go to Home</Link> import {Link} from react-router-dom redireact the rout*/}
+          {/* handleSubmit = () => {this.props.history.push('/house')} redirect after submit*/}
+          {/* <Link to='/home'>Go to Home</Link> */}
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
