734165940f4b7dce13b2e4dcddbb63046568e4df
estherkimyunjung <estherkimyunjung@gmail.com>
2020-07-23 18:26:50 -0500

    create detail component

---
+++ src/component/PropertyDetail.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class PropertyDetail extends Component {
+  state = {  }
+  render() { 
+    return ( 
+      <h1>PropertyDetail</h1>
+     );
+  }
+}
+ 
+export default PropertyDetail;
