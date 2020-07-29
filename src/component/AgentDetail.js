183c6e4170dfd3474abafd924ec1a963eaef4a51
estherkimyunjung <estherkimyunjung@gmail.com>
2020-07-29 03:07:42 -0500

    speedDail is not working so change to popup

--- src/component/AgentDetail.js
+++ src/component/AgentDetail.js
@@ -1,72 +1,24 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import Backdrop from '@material-ui/core/Backdrop';
-import SpeedDial from '@material-ui/lab/SpeedDial';
-import SpeedDialIcon from '@material-ui/lab/SpeedDialIcon';
-import SpeedDialAction from '@material-ui/lab/SpeedDialAction';
-import RecentActorsIcon from '@material-ui/icons/RecentActors';
-import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import React from 'react'
+import { Popup, Card, Image, Rating, Message } from 'semantic-ui-react'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: 180,
-    transform: 'translateZ(0px)',
-    flexGrow: 1,
-  },
-  speedDial: {
-    position: 'absolute',
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
-  },
-}));
+const PopupExampleTrigger = (props) => (
+  <Popup
+    trigger={
+      <Card>
+        <Card.Content>
+          <Card.Header>Addtional Information</Card.Header>
+        </Card.Content>
+      </Card>
+    }
+  >
+    <Popup.Header>Agent Information</Popup.Header>
+    <Popup.Content>
+   <Message>
+      <h5>Experience : {props.agent.experienceyears} yrs</h5>
+      <h5>License # : {props.agent.license} </h5>
+    </Message>
+    </Popup.Content>
+  </Popup>
+)
 
-export default function SpeedDialTooltipOpen(props) {
-  const actions = [
-    { icon: <AccessTimeIcon/>, name: 'Experience' + ' ' + props.agent.experienceyears +'yrs' },
-    { icon: <RecentActorsIcon/>, name: 'License #' + props.agent.license },
-  ];
-  
-  console.log("AGENTDDD", props)
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-  const [hidden, setHidden] = React.useState(false);
-
-  const handleVisibility = () => {
-    setHidden((prevHidden) => !prevHidden);
-  };
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  return (
-    <div className={classes.root}>
-      <Button onClick={handleVisibility}>Additional Information</Button>
-      <Backdrop open={open} />
-      <SpeedDial style={{ height:'5px'}}
-        ariaLabel="SpeedDial tooltip example"
-        className={classes.speedDial}
-        hidden={hidden}
-        icon={<SpeedDialIcon />}
-        onClose={handleClose}
-        onOpen={handleOpen}
-        open={open}
-      >
-        {actions.map((action) => (
-          <SpeedDialAction
-            key={action.name}
-            icon={action.icon}
-            tooltipTitle={action.name}
-            tooltipOpen
-            onClick={handleClose}
-          />
-        ))}
-      </SpeedDial>
-    </div>
-  );
-}
+export default PopupExampleTrigger
